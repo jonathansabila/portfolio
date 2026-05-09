f5fea35bda12750d438d2d8209f0891552a82d52
jonathansabila <jonathan.sabila@sdca.edu.ph>
2026-05-09 10:27:34 +0800

    Modify Files

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+/* Toggle menu open/close */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  navbar.classList.toggle('active');
+};
+
+
+function openModal(imgSrc) {
+  document.getElementById("certModal").style.display = "block";
+  document.getElementById("modalImg").src = imgSrc;
+}
+
+function closeModal() {
+  document.getElementById("certModal").style.display = "none";
+}
+
+/* Optional: close modal with ESC key */
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
